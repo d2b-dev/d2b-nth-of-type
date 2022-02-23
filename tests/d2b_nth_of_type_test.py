0ea252e7dbf206d20b859c536156541df987dead
Andrew Ross <andrew.ross.mail@gmail.com>
2022-02-23 13:29:43 -0500

    Update tests in accordance with changes

--- tests/d2b_nth_of_type_test.py
+++ tests/d2b_nth_of_type_test.py
@@ -11,15 +11,16 @@ from d2b_nth_of_type import add_arguments
 from d2b_nth_of_type import filter_files
 from d2b_nth_of_type import group_sidecars
 from d2b_nth_of_type import load_sidecars
-from d2b_nth_of_type import parse_group_keys
-from d2b_nth_of_type import parse_sort_key
+from d2b_nth_of_type import parse_groupby
+from d2b_nth_of_type import parse_sortby
 from d2b_nth_of_type import Sidecar
 from d2b_nth_of_type import sort_sidecars
+from d2b_nth_of_type import SortConfig
 
 
 class SidecarData(NamedTuple):
     filename: str
-    series_number: int
+    series_number: int | str | None
     series_description: str
     phase_encoding_direction: str
 
@@ -31,16 +32,19 @@ def fake_sidecar_files(tmpdir: str) -> list[Path]:
         SidecarData("b.json", 1, "desc3", "j"),
         SidecarData("c.json", 3, "desc2", "i"),
         SidecarData("a.json", 1, "desc1", "i"),
+        SidecarData("f.json", None, "desc3", "k"),
+        SidecarData("e.json", "13", "desc2", "j"),
     ]
 
     files: list[Path] = []
     for scd in sidecar_data:
+        data = {}
+        if scd.series_number is not None:
+            data["SeriesNumber"] = scd.series_number
+        data["SeriesDescription"] = scd.series_description
+        data["PhaseEncodingDirection"] = scd.phase_encoding_direction
+
         fp = Path(tmpdir) / scd.filename
-        data = {
-            "SeriesNumber": scd.series_number,
-            "SeriesDescription": scd.series_description,
-            "PhaseEncodingDirection": scd.phase_encoding_direction,
-        }
         fp.write_text(json.dumps(data))
         files.append(fp)
 
@@ -83,53 +87,59 @@ def test_load_sidecars(fake_sidecar_files: list[Path]):
 
     for sidecar, original_fp in zip(sidecars, fake_sidecar_files):
         assert sidecar.path == original_fp
-        assert "SeriesNumber" in sidecar.data
         assert "SeriesDescription" in sidecar.data
         assert "PhaseEncodingDirection" in sidecar.data
 
 
 def test_sort_sidecars_asc(fake_sidecars: list[Sidecar]):
-    sort_key = parse_sort_key("SeriesNumber:asc")
-    ordered = sort_sidecars(fake_sidecars, sort_key=sort_key)
+    prop, reverse = parse_sortby("SeriesNumber:asc")
+    sort_config = SortConfig.infer_from_sidecars(fake_sidecars, prop, reverse)
+    ordered = sort_sidecars(fake_sidecars, sort_config=sort_config)
 
     first = ordered[0]
     last = ordered[-1]
 
     assert first.path.name == "a.json"
     assert first.data["SeriesNumber"] == 1
-    assert last.path.name == "c.json"
-    assert last.data["SeriesNumber"] == 3
+    assert last.path.name == "f.json"
 
 
 def test_sort_sidecars_desc(fake_sidecars: list[Sidecar]):
-    sort_key = parse_sort_key("SeriesDescription:desc")
-    ordered = sort_sidecars(fake_sidecars, sort_key=sort_key)
+    prop, reverse = parse_sortby("SeriesDescription:desc")
+    sort_config = SortConfig.infer_from_sidecars(fake_sidecars, prop, reverse)
+    ordered = sort_sidecars(fake_sidecars, sort_config=sort_config)
 
     first = ordered[0]
     last = ordered[-1]
 
-    assert first.path.name == "b.json"
+    assert first.path.name == "f.json"
     assert first.data["SeriesDescription"] == "desc3"
     assert last.path.name == "a.json"
     assert last.data["SeriesDescription"] == "desc1"
 
 
 def test_group_sidecars_single_key(fake_sidecars: list[Sidecar]):
-    sort_key = parse_sort_key("SeriesNumber:asc")
-    ordered = sort_sidecars(fake_sidecars, sort_key)
+    prop, reverse = parse_sortby("SeriesNumber:asc")
+    sort_config = SortConfig.infer_from_sidecars(fake_sidecars, prop, reverse)
+    ordered = sort_sidecars(fake_sidecars, sort_config=sort_config)
 
-    group_keys = parse_group_keys("SeriesDescription")
+    group_keys = parse_groupby("SeriesDescription")
     grouped = group_sidecars(ordered, group_keys=group_keys)
 
     assert [sc.path.name for sc in grouped[("desc1",)]] == ["a.json"]
-    assert [sc.path.name for sc in grouped[("desc2",)]] == ["d.json", "c.json"]
+    assert [sc.path.name for sc in grouped[("desc2",)]] == [
+        "d.json",
+        "c.json",
+        "e.json",
+    ]
 
 
 def test_group_sidecars_multiple_keys(fake_sidecars: list[Sidecar]):
-    sort_key = parse_sort_key("SeriesNumber:asc")
-    ordered = sort_sidecars(fake_sidecars, sort_key)
+    prop, reverse = parse_sortby("SeriesNumber:asc")
+    sort_config = SortConfig.infer_from_sidecars(fake_sidecars, prop, reverse)
+    ordered = sort_sidecars(fake_sidecars, sort_config=sort_config)
 
-    group_keys = parse_group_keys("SeriesDescription,,SeriesNumber")
+    group_keys = parse_groupby("SeriesDescription,,SeriesNumber")
     grouped = group_sidecars(ordered, group_keys=group_keys)
 
     # notice that the empty group key is dropped and that each group key
